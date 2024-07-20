7609b6992e3e20e7ed44ef6ebe46a93306516feb
Lebo Molaiwa <molaiwasharon@gmail.com>
2024-07-20 20:51:57 +0200

    updated js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Create a remove button
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
-    removeButton.classList.add("remove-btn");
+    removeButton.className = "remove-btn";
 
     // Assign an onclick event to remove the list item
     removeButton.onclick = function () {
